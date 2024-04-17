dd098fb7c298663d648dcc8703e535db66b89169
yuj2n <dbwls4595@naver.com>
2024-04-17 12:48:33 +0900

    quick_sort 구현 완성

--- src/git_test3a/Sort.java
+++ src/git_test3a/Sort.java
@@ -5,6 +5,27 @@ public class Sort {
 		quickSort(a, 0, a.length - 1);
 	}
 
-	static void swap(int[] a, int i, int j) { int temp = a[i]; a[i] = a[j]; a[j] = temp; }
+	static void swap(int[] a, int i, int j) {
+		int temp = a[i];
+		a[i] = a[j];
+		a[j] = temp;
+	}
 
-static int partition(int[] a, int start, int end) { // TODO: partition 알고리즘을 구현해야 함 } static void quickSort(int[] a, int start, int end) { if (start >= end) return; int middle = partition(a, start, end); quickSort(a, start, middle-1); quickSort(a, middle+1, end); } }
+	static int partition(int[] a, int start, int end) {
+		int value = a[end];
+		int i = start - 1;
+		for (int j = start; j <= end - 1; ++j)
+			if (a[j] < value)
+				swap(a, ++i, j);
+		swap(a, i + 1, end);
+		return i + 1;
+	}
+
+	static void quickSort(int[] a, int start, int end) {
+		if (start >= end)
+			return;
+		int middle = partition(a, start, end);
+		quickSort(a, start, middle - 1);
+		quickSort(a, middle + 1, end);
+	}
+}
